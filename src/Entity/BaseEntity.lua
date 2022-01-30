658577319987c1e2995e871ce71798663a8bdd1b
lukehayes <lukepurplehayes@gmail.com>
2022-01-30 21:08:33 +0000

    Updated Ball movement to use delta time and added angles.

--- src/Entity/BaseEntity.lua
+++ src/Entity/BaseEntity.lua
@@ -6,9 +6,11 @@ function BaseEntity:new(x,y)
     self.y = y
     self.width = 10
     self.height = 10
-    self.speed = 5
+    self.speed = 50
     self.xDir = 1
     self.yDir = -1
+    self.xAngle = 0
+    self.yAngle = 0
     self.renderMode = "fill"
     return self
 end
@@ -29,22 +31,26 @@ function BaseEntity:update(dt)
     --Check if ball hits edge of screen
     if self.x > 780 then
         self.xDir = self.xDir - 1
+        self.xAngle = love.math.random()
     end
 
     if self.x < 0 then
         self.xDir = self.xDir + 1
+        self.xAngle = love.math.random()
     end
 
     if self.y > 580 then
-        self.xDir = self.xDir - 1
+        self.yDir = self.yDir - 1
+        self.yAngle = love.math.random()
     end
 
     if self.y < 0 then
         self.yDir = self.yDir + 1
+        self.yAngle = love.math.random()
     end
 
-    self.x = self.x + self.xDir * self.speed
-    self.y = self.y + self.xDir * self.speed
+    self.x = self.x + self.xDir * self.speed * dt
+    self.y = self.y + self.yDir * self.speed * dt
 end
 
 return BaseEntity
