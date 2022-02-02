f88127f5f3714f74783ccb01abcbaed3d6bb17ea
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-02 18:09:32 +0000

    Added Asset class static helper methods.

--- src/Asset/Asset.lua
+++ src/Asset/Asset.lua
@@ -1,12 +1,35 @@
---Base class for all on-screen entities.
-local Asset = {}
+--- Asset Helper Functions
+--  @module Asset
+local Object = require "src/Libs/classic"
+Asset = Object:extend()
 
-function Asset:new()
-    return self
+-- STATIC METHODS
+
+--- Check if a file exists.
+--  @param file    The name of the file to check existance of.
+--  @return        Full path to image file.
+function Asset.exists(file)
+    return love.filesystem.getInfo(file)
 end
 
-function Asset:exists(file)
-    return love.filesystem.getInfo(file)
+--- Get the assets directory string.
+--  @return    A string for the assets folder ending in forward slash.
+function Asset.assetsDir()
+    return "assets/"
+end
+
+--- Get a file from the images directory. ".png" added to end.
+--  @param file    The name of the image file to get.
+--  @return        Full path to sound file.
+function Asset.image(file)
+    return "assets/images/" .. file .. ".png"
+end
+
+--- Get a file from the sounds directory. ".mp3" added to end.
+--  @param file    The name of the sound file to get.
+--  @return        Full path to sound file.
+function Asset.sound(file)
+    return "assets/sounds/" .. file .. ".mp3"
 end
 
 return Asset
