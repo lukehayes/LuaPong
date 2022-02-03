7099f93c326eae5ceaf181491fea7ee7e1b064e4
Luke Hayes <lukepurplehayes@gmail.com>
2022-02-03 10:08:56 +0000

    Ball object now created in love.load().

--- main.lua
+++ main.lua
@@ -1,8 +1,13 @@
 local Ball = require "src/Pong/Ball"
 
+function love:load()
+    b = Ball(400,400)
+end
 
 function love.draw()
+    b:draw()
 end
 
 function love.update(dt)
+    b:update(dt)
 end
